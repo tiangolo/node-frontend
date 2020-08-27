737f3c6860cfedaca4b4fa8deb1f13ebdf63cad4
yoprogramo <yoprogramo@gmail.com>
2020-08-27 12:09:23 +0200

    V12 al repositorio

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -5,10 +5,10 @@ set -x
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t tiangolo/node-frontend:10 .
+docker build -t tiangolo/node-frontend:12 .
 
 docker build -t tiangolo/node-frontend:latest .
 
-docker push tiangolo/node-frontend:10
+docker push tiangolo/node-frontend:12
 
 docker push tiangolo/node-frontend:latest
